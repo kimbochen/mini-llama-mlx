df4c57bb4b3379d209ccbaa5ea92687e08b2bf07
kimbochen <chentenghung@gmail.com>
2024-04-15 12:56:01 -0700

    Initial commit. Trainer incomplete.

---
+++ dataset.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+import mlx.core as mx
+from datasets import load_dataset
+from sentencepiece import SentencePieceProcessor
+from tqdm import tqdm
+
+DATASET_DIR = 'wikitext_data/train/'
+
+
+def create_wikitext_dataset():
+    print(f'Creating WikiText dataset at {DATASET_DIR}...')
+    dataset = load_dataset('wikitext', 'wikitext-103-v1')
+
+    examples = []
+    example = train_text[1]  # train_text[0] is emtpy
+    for text in train_text[2:]:
+        if (text[:3] == ' = ' and text[-4:] == ' = \n') and text[3].isupper():
+            examples.append(example)
+            example = text
+        else:
+            example += text
+
+    sp_model = SentencePieceProcessor(model_file='tokenizer.model')
+    def encode(text):
+        tokens = sp_model.encode(text, add_bos=True, add_eos=True)
+        token_arr = mx.array(tokens, dtype=mx.uint16)
+        return token_arr
+    train_tokens = [encode(example) for example in tqdm(examples)]
+
+    chunk_size = 1000
+    for chunk_idx, idx in enumerate(range(0, len(train_tokens), chunk_size)):
+        mx.savez(f'{DATASET_DIR}/example_chunk{chunk_idx:02d}', *train_tokens[idx:idx+chunk_size])
+
+
+def config_dataloader(bsz, seq_len, pad_token_id):
+    train_data_dir = Path(DATASET_DIR)
+    assert train_data_dir.exists(), f'Invalid path {train_data_dir}; pwd: {Path("./").absolute()}'
+    train_examples = []
+    for ex_path in sorted(train_data_dir.glob('*.npz')):
+        train_examples.extend(mx.load(str(ex_path)).values())
+
+    blk_size = seq_len + 1
+    pad_example = lambda ex: mx.pad(ex, [0, blk_size - ex.size % blk_size], pad_token_id)
+    train_examples = [*map(pad_example, train_examples)]
+    train_examples = mx.concatenate(train_examples, axis=0)
+
+    bblk_size = bsz * blk_size  # Batch block size
+    n_batches = len(train_examples) - blk_size + 1
+
+    def load_data_():
+        for i in range(n_batches):
+            bblk = train_examples[i:i+bblk_size].reshape([bsz, blk_size])
+            yield bblk[:, :-1], bblk[:, 1:]
+
+    return load_data_
+
+
+if __name__ == '__main__':
+    load_data = config_dataloader(4, 512, -1)
+    xb, yb = next(load_data())
+    print(xb, yb, sep='\n')
---
+++ llama.py
@@ -0,0 +1,112 @@
+from dataclasses import dataclass, asdict
+
+import mlx.core as mx
+from mlx import nn
+from mlx.nn import MultiHeadAttention as MHA
+from mlx.core.fast import scaled_dot_product_attention
+
+
+
+@dataclass
+class LLaMAConfig:
+    '''
+    Model configuration reference:
+    https://github.com/epfml/llm-baselines?tab=readme-ov-file#results-on-wikitext
+    '''
+    n_layers: int = 24
+    vocab_size: int = 32000  # V , LLaMA 2 tokenizer
+    d_embd: int = 768        # D
+    n_heads: int = 12        # N
+    seq_len: int = 512       # T
+    rope_theta: float = 1e4
+    rope_scale: float = 1.0
+    ffn_mult: int = 256
+    norm_eps: float = 1e-5
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, d_embd, n_heads, rope_theta, rope_scale, **kwargs):
+        super().__init__()
+        assert d_embd % n_heads == 0
+        self.d_head = d_embd // n_heads  # H
+
+        self.attn_proj = nn.Linear(d_embd, 3*d_embd, bias=False)
+        self.rope = nn.RoPE(self.d_head, base=rope_theta, scale=rope_scale)
+        self.scale = self.d_head ** -0.5
+        self.out_proj = nn.Linear(d_embd, d_embd, bias=False)
+
+    def __call__(self, x_BTD, mask_TT):
+        B, T, D = x_BTD.shape
+        to_attn_heads = lambda z: z.reshape(B, T, -1, self.d_head).transpose(0, 2, 1, 3)
+
+        qkv_BTD = self.attn_proj(x_BTD).split(3, axis=-1)
+        Q_BNTH, K_BNTH, V_BNTH = map(to_attn_heads, qkv_BTD)
+        Q_BNTH, K_BNTH = self.rope(Q_BNTH), self.rope(K_BNTH)
+        O_BNTH = scaled_dot_product_attention(Q_BNTH, K_BNTH, V_BNTH, scale=self.scale, mask=mask_TT)
+        out_BTD = self.out_proj(O_BNTH.transpose(0, 2, 1, 3).reshape(B, T, D))
+
+        return out_BTD
+
+
+class FeedForwardNet(nn.Module):
+    def __init__(self, d_embd, ffn_mult, **kwargs):
+        super().__init__()
+        hidden_dim = int((4 * d_embd) * 2 / 3)  # C
+        hidden_dim = ffn_mult * ((hidden_dim + ffn_mult - 1) // ffn_mult)  # The next multiple of ffn_mult
+
+        self.gate_proj = nn.Linear(d_embd, hidden_dim, bias=False)
+        self.up_proj = nn.Linear(d_embd, hidden_dim, bias=False)
+        self.down_proj = nn.Linear(hidden_dim, d_embd, bias=False)
+
+    def __call__(self, x_BTD):
+        h_BTC = nn.silu(self.gate_proj(x_BTD)) * self.up_proj(x_BTD)  # SwiGLU
+        out_BTD = self.down_proj(h_BTC)
+        return out_BTD
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, d_embd, norm_eps, **kwargs):
+        super().__init__()
+        self.pre_norm = nn.RMSNorm(d_embd, norm_eps)
+        self.attn = SelfAttention(d_embd=d_embd, **kwargs)
+        self.ffn_norm = nn.RMSNorm(d_embd, norm_eps)
+        self.ffn = FeedForwardNet(d_embd=d_embd, **kwargs)
+
+    def __call__(self, x_BTD, mask_TT):
+        h_BTD = x_BTD + self.attn(self.pre_norm(x_BTD), mask_TT)
+        out_BTD = h_BTD + self.ffn(self.ffn_norm(h_BTD))
+        return out_BTD
+
+
+class LLaMA(nn.Module):
+    def __init__(self, vocab_size, n_layers, d_embd, norm_eps, **kwargs):
+        super().__init__()
+        self.embd_toks = nn.Embedding(vocab_size, d_embd)
+        self.layers = [
+            TransformerBlock(d_embd=d_embd, norm_eps=norm_eps,**kwargs)
+            for _ in range(n_layers)
+        ]
+        self.out_norm = nn.RMSNorm(d_embd, norm_eps)
+        self.lm_head = nn.Linear(d_embd, vocab_size, bias=False)
+
+    def __call__(self, tok_idxs_BT):
+        h_BTD = self.embd_toks(tok_idxs_BT)
+
+        causal_mask_TT = MHA.create_additive_causal_mask(h_BTD.shape[1])
+        for layer in self.layers:
+            h_BTD = layer(h_BTD, causal_mask_TT)
+        h_BTD = self.out_norm(h_BTD)
+
+        logits_BTV = self.lm_head(h_BTD)
+
+        return logits_BTV
+
+
+if __name__ == '__main__':
+    cfg_m = LLaMAConfig()
+    model = LLaMA(**asdict(cfg_m))
+
+    mx.random.seed(3985)
+    tok_idxs = mx.random.randint(0, cfg_m.vocab_size, shape=[2, cfg_m.seq_len])
+
+    print(model(tok_idxs).shape)
