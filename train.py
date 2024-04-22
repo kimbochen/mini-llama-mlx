fbd70005d46f490d7f3362d0a0d42e993bc97a8a
kimbochen <chentenghung@gmail.com>
2024-04-21 22:30:12 -0700

    Added gradient accumulation.

--- train.py
+++ train.py
@@ -17,8 +17,9 @@ from llama import init_params, LLaMAConfig, LLaMA
 class TrainerConfig:
     bsz: int = 16
     lr: float = 1e-3
-    n_steps: int = 25000
-    warmup_steps: int = 2500  # 10%
+    n_steps: int = 112 * 16
+    warmup_steps: int = 11
+    grad_acc_steps: int = 16
     pad_token_id: int = -1
     ckpt_name: str = 'mini-llama-wikitext-bsl'
 
@@ -28,7 +29,8 @@ def train():
     cfg_m = LLaMAConfig(n_layers=6, d_embd=256, n_heads=8)
     wandb.init(project='mini-llama-mlx', config={**asdict(cfg_t), **asdict(cfg_m)})
 
-    train_dl = config_dataloader(**asdict(cfg_t), split='train', seq_len=cfg_m.seq_len)
+    load_train_data = config_dataloader(**asdict(cfg_t), **asdict(cfg_m), split='train')
+
     model = init_params(LLaMA(**asdict(cfg_m)))
 
     lr_schedule = optim.join_schedules([
@@ -46,23 +48,32 @@ def train():
         return loss
 
 
-    state = [model.state, optimizer.state]
-    @partial(mx.compile, inputs=state, outputs=state)
-    def train_step(inputs_BT, labels_BT):
+    @partial(mx.compile, inputs=model.state, outputs=model.state)
+    def train_step(inputs_BT, labels_BT, grads):
         loss_and_grad = nn.value_and_grad(model, train_forward_pass)
-        loss, grads = loss_and_grad(model, inputs_BT, labels_BT)
-        optimizer.update(model, grads)
-        return loss
+        loss, grads_m = loss_and_grad(model, inputs_BT, labels_BT)
+        grads = tree_map(lambda g, gm: (g + gm / cfg_t.grad_acc_steps), grads, grads_m)
+        return loss, grads
 
 
+    grads = tree_map(lambda p: mx.zeros(p.shape), model)
+    pbar = tqdm(total=cfg_t.n_steps)
     model.train()
-    for inputs_BT, labels_BT in (pbar := tqdm(train_dl(), total=cfg_t.n_steps)):
+
+    for step, (inputs_BT, labels_BT) in enumerate(load_train_data()):
         try:
-            loss = train_step(inputs_BT, labels_BT)
-            mx.eval(state, loss)
+            loss, grads = train_step(inputs_BT, labels_BT, grads)
+            mx.eval(loss, grads)
+
+            if ((step + 1) % cfg_t.grad_acc_steps == 0) or (step == cfg_t.n_steps - 1):
+                optimizer.update(model, grads)
+                mx.eval(model.state, optimizer.state)
+                grads = tree_map(lambda p: mx.zeros(p.shape), model)
+
             loss, lr = map(lambda x: x.item(), [loss, optimizer.learning_rate])
             wandb.log({'loss': loss, 'learning_rate': lr})
             pbar.set_description(f'{loss=:.4f} | {lr=:.2e} | peak_mem={(mx.metal.get_peak_memory()/2**30):.2f}GB')
+            pbar.update(1)
         except KeyboardInterrupt:
             break
 
